578bc1c505a96d174d8928ba5b9876938ac9417d
Anton Sheiko <antonstendal19@gmail.com>
2024-11-03 20:11:35 +0100

    Learn findElement by tag Created list<WebElement>

--- src/test/java/SelectorTest.java
+++ src/test/java/SelectorTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.sql.DriverManager;
+import java.util.List;
 
 public class SelectorTest {
 
@@ -16,13 +17,22 @@ public class SelectorTest {
         driver.manage().window().maximize();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
 
-
+        //by id
         driver.findElement(By.id("clickOnMe"));
 
         //by name
-
         By firstName = By.name("fname");
-        WebElement firstNameInput = driver.findElement(firstName);
-        driver.findElement(By.name("fname"));
+        driver.findElement(firstName);
+
+        //by className
+        By paraHidden = By.className("topSecret");
+        driver.findElement(paraHidden);
+
+        By inputLocator = By.tagName("input");
+        WebElement input = driver.findElement(inputLocator);
+        input.sendKeys("First element");
+        List<WebElement> inputs = driver.findElements(inputLocator);
+        System.out.println(inputs.size());
+
     }
 }
