073b3836ed5116452aadd7c604120de7cbd56506
Anton Sheiko <antonstendal19@gmail.com>
2024-11-03 06:10:33 +0100

    First test completed. Learn new methods: .swithTo() .sendKeys(Keys.ENTER) .defaultContent() .findElement()
    Learn
    Search By.name
    Assert.assertTrue(.isDisplayed())

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,13 +1,11 @@
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.InvalidArgumentException;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
@@ -16,13 +14,26 @@ public class SeleniumTest {
 
         WebDriver driver = getDrivers("chrome");
         driver.manage().window().maximize();
-        Dimension windowSize = new Dimension(400, 400);
-        driver.manage().window().setSize(windowSize);
         driver.get("https://www.google.com/");
-        JavascriptExecutor executor = (JavascriptExecutor) driver;
-        executor.executeScript("window.open('https://mvnrepository.com/artifact/org.testng/testng/7.10.2','blank')");
+        //przehodzimy do frame cookies
+        //driver.switchTo().frame(0);
+        //find the button
+        WebElement agreeButton = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]"));
+        agreeButton.click();
+        //came back searching field
+        driver.switchTo().defaultContent();
+        //fins search name
+        WebElement searchField = driver.findElement(By.name("q"));
+        //enter value search "Selenium" to field
+        searchField.sendKeys("Selenium");
+        //push the button Enter
+        searchField.sendKeys(Keys.ENTER);
+        //find results
+        WebElement result = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/div/div/div/div[2]/cite"));
 
-        driver.quit(); // or .close()
+        Assert.assertTrue(result.isDisplayed());
+
+
     }
 
     public WebDriver getDrivers(String browser) {
