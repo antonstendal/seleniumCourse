40d4a9f53d42b96e6d076f337db8a383d0c489c0
Anton Sheiko <antonstendal19@gmail.com>
2024-11-12 06:14:30 +0100

    Learn how works DoubleClick Add class ActionsTest

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -1,8 +1,10 @@
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -21,6 +23,11 @@ public class UploadTest {
         String fileName = "beforeUpload" + randomNumber + ".png";
         FileUtils.copyFile(before, new File("src/test/resources/" + fileName));
         driver.findElement(By.id("myFile")).sendKeys("D:\\Anton\\ExampleTestForQA.txt");
+
+        Actions actions = new Actions(driver);
+        actions.contextClick().perform();
+        actions.contextClick(driver.findElement(By.id("myFile"))).perform();
+
 
 
     }
