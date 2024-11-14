e25ad9b41adf693fc321d91bc53c0bd1964556fd
Anton Sheiko <antonstendal19@gmail.com>
2024-11-14 09:59:17 +0100

    How to check Wait elements Learn implicitlyWait()

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class WaitTest {
 
     WebDriver driver;
 
     @Test
-    public void waitTest() throws InterruptedException {
+    public void waitTest() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
 
         driver.findElement(By.tagName("button")).click();
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
         driver.findElement(By.cssSelector("p"));
 
     }
